d07c6e3d435dd16d0ba23b13aa2779ed4d002f1b
poisonalien <anandmt3@gmail.com>
2020-07-27 18:01:36 +0200

    Code cleanup and return MAF objects

--- R/tcga_load.R
+++ R/tcga_load.R
@@ -3,60 +3,51 @@
 #' @description Loads a user mentioned TCGA cohort into global enviornment
 #' @param study Study names to load. Use  \code{\link{tcga_available}} to see available options.
 #' @param source Can be \code{MC3} or \code{Firehose}. Default \code{MC3}
-#' @param reassign If `TRUE`, return a \code{MAF} object instread of loading data into global environment.
 #' It will be usefull if you want to operate multiple \code{MAF}s.
-#' @import maftools
+#' @import maftools data.table
 #' @examples
 #' tcga_load(study = "LAML") #Loads TCGA LAML cohort
 #' @export
 #' @seealso \code{\link{tcga_available}}
 #'
-tcga_load = function(study = NULL, source = "MC3", reassign = TRUE){
+tcga_load = function(study = NULL, source = "MC3"){
+
 
   if(is.null(study)){
-    stop("Please provide a study name. Use tcga_available() to see available cohorts.")
+    stop("Please provide a study name. Use tcga_available() for available cohorts.")
   }
 
+  source = match.arg(arg = source, choices = c("MC3", "Firehose"))
+
   study = toupper(x = study)
-  if (length(study) > 1) {
-    return(invisible(setNames(lapply(study, tcga_load, reassign = reassign), study)))
+  cohorts = system.file('extdata', 'cohorts.txt', package = 'TCGAmutations')
+  cohorts = data.table::fread(file = cohorts)
+  cohorts = cohorts[cohorts$Study_Abbreviation %in% study]
+
+  if(nrow(cohorts) == 0){
+    stop("Could not find requested datasets!\nUse tcga_available() for available cohorts.")
   }
 
   if(source == "MC3"){
-    study.dat = system.file('extdata/MC3/', paste0(study, ".RData"), package = 'TCGAmutations')
-    doi = "https://doi.org/10.1016/j.cels.2018.03.002"
-  }else if(source == "Firehose"){
-    study.dat = system.file('extdata/Firehose/', paste0(study, ".RData"), package = 'TCGAmutations')
-    if(study.dat != ""){
-      cohorts = system.file('extdata', 'cohorts.txt', package = 'TCGAmutations')
-      cohorts = data.table::fread(input = cohorts, data.table = FALSE)
-      doi = cohorts[cohorts$Study_Abbreviation %in% study, "Firehose"]
-      doi = unlist(data.table::tstrsplit(x = doi, spli = "\\[", keep = 2))
-      doi = gsub(pattern = "\\]$", replacement = "",
-                 x = doi)
-    }
+    study.dat = system.file('extdata/MC3/', paste0(cohorts$Study_Abbreviation, ".RDs"), package = 'TCGAmutations')
+    doi = rep("https://doi.org/10.1016/j.cels.2018.03.002", nrow(cohorts))
   }else{
-    stop("source can only be MC3 or Firehose")
+    study.dat = system.file('extdata/Firehose/', paste0(cohorts$Study_Abbreviation, ".RDs"), package = 'TCGAmutations')
+    doi = cohorts$Firehose
+    doi = unlist(data.table::tstrsplit(x = doi, spli = "\\[", keep = 2))
+    doi = gsub(pattern = "\\]$", replacement = "", x = doi)
   }
 
-
-  if(study.dat == ""){
-    stop(study, " not available. Use tcga_available() to see available cohorts.")
-  } else{
-    if (isFALSE(reassign)) {
-      load(file = study.dat, verbose = TRUE, envir = .GlobalEnv)
-      message(paste0("Successfully loaded TCGA ", study, "!"))
-    } else {
-      load(file = study.dat, verbose = FALSE, envir = environment())
-    }
-    #message(paste0("See MAF object tcga_", tolower(study)))
-    message(paste0("Please cite ", doi , " for MAF source."))
+  if (length(study.dat) > 1) {
+    mafs = lapply(seq_along(study.dat), function(i){
+      message("Loading ", cohorts$Study_Abbreviation[i], ". Please cite: ", doi[i], " for reference")
+      readRDS(file = study.dat[i])
+    })
+    names(mafs) = cohorts$Study_Abbreviation
+  }else{
+    message("Loading ", cohorts$Study_Abbreviation, ". Please cite: ", doi, " for reference")
+    mafs = readRDS(file = study.dat)
   }
 
-  maf <- ls(pattern = "tcga")
-  if (length(maf) == 0) {
-    return(invisible(NULL))
-  } else {
-    return(get(maf))
-  }
+  mafs
 }
